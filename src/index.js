652c9c0c3f0f723cbec026d1fb0eb8f8b63ea54e
sumancrest0001 <suman.crest0001@gmail.com>
2020-03-05 19:26:34 +0545

    initial setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import PropTypes from 'prop-types';
+
+ReactDOM.render(<div />, document.getElementById('root'));
+
