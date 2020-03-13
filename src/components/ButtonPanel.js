02b6f4bec755751cdc75668fcf17116deb49b186
sumancrest0001 <suman.crest0001@gmail.com>
2020-03-13 23:38:59 +0545

    improved the code

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,108 +4,106 @@ import Button from './Button';
 import classes from './ButtonPanel.module.css';
 
 const ButtonPanel = ({ click }) => {
-  const handleClick = buttonName => click(buttonName);
-
   return (
     <div className={classes.ButtonPanel}>
       <div className={classes.calcRow}>
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           name="AC"
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           name="+/-"
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           name="%"
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           name="÷"
         />
       </div>
       <div className={classes.calcRow}>
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           name="7"
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           name="8"
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           name="9"
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           name="X"
         />
       </div>
       <div className={classes.calcRow}>
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           name="4"
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           name="5"
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           name="6"
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           name="+"
         />
       </div>
       <div className={classes.calcRow}>
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           name="1"
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           name="2"
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           name="3"
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           name="-"
         />
       </div>
       <div className={classes.calcRow}>
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           wide
           name="0"
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           color="#ebe8e8"
           name="."
         />
         <Button
-          clicked={handleClick}
+          clicked={click}
           name="="
         />
       </div>
